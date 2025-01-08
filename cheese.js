ac7d605e4d52d6f8d96da156b00128c60cdc7860
Andrew Feasel <feaseand000@student.evergreenps.org>
2025-01-08 13:28:52 -0800

    Create cheese.js

---
+++ cheese.js
@@ -0,0 +1,7 @@
+const counter = document.getElementById('counter');
+const cheese = document.getElementById('cheese');
+cheese.addEventListener('click', () => {
+  let int = counter.textContent;
+  int++;
+  //do something with audio
+})
