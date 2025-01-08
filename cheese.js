ba939f6c5c83ae74c15e1127b717c9858150e335
Andrew Feasel <feaseand000@student.evergreenps.org>
2025-01-08 13:40:04 -0800

    Update cheese.js

--- cheese.js
+++ cheese.js
@@ -1,8 +1,10 @@
-const counter = document.getElementById('counter');
-const cheese = document.getElementById('cheese');
-cheese.onclick = () => {
+document.addEventListener('DOMContentLoaded', () => {
+  const counter = document.getElementById('counter');
+  const cheese = document.getElementById('cheese');
+  cheese.onclick = () => {
   let int = counter.textContent;
   alert(typeof int);
   int++;
   //do something with audio
 }
+});
