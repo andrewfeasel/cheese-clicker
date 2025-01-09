36ae855e08c882ef6a4d0a33c5f92b01374287a9
Andrew Feasel <feaseand000@student.evergreenps.org>
2025-01-09 10:24:25 -0800

    Update cheese.js

--- cheese.js
+++ cheese.js
@@ -1,12 +1,14 @@
 const sfx = new Audio('videoplayback');
 document.addEventListener('DOMContentLoaded', () => {
-  const counter = document.getElementById('counter');
-  const cheese = document.getElementById('cheese');
-  let int = 0;
-  counter.textContent = `${int} clicks`;
-  cheese.onclick = () => {
-    int++;
+  sfx.addEventListener("canplaythrough", (event) => {
+    const counter = document.getElementById('counter');
+    const cheese = document.getElementById('cheese');
+    let int = 0;
     counter.textContent = `${int} clicks`;
-    sfx.play();
-  }
+    cheese.onclick = () => {
+      int++;
+      counter.textContent = `${int} clicks`;
+      sfx.play();
+    }
+  });
 });
